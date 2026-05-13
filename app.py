721de686391dc1918a185362b5691cdeaeb830e9
karenashraff <karenashraff90@gmail.com>
2026-05-14 00:45:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if st.button("Generate Plan 🚀"):
             "Content-Type": "application/json"
         }
         payload = {
-            "model": "llama3-8b-8192",
+           "model": "llama-3.3-70b-versatile",
             "messages": [
                 {
                     "role": "user",
